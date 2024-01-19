284d403c5a5b201e730b549b577e9c7d17e91f22
Informatica <informatica@alumno.local>
2024-01-19 12:02:20 -0600

    Promesas

--- assets/logica.js
+++ assets/logica.js
@@ -1,29 +1,34 @@
-let valorPantalla = '';
-
-function limpiarPantalla(){
-    valorPantalla = '';
-    actualizarPantalla();
-}
-
-function agregarCaracter(char){
-
-    valorPantalla += char;
-    actualizarPantalla();
-
+function enviarDatos(){
+    const nombre = document.getElementById('nombre').value;
+    enviarDatosAlServidor(nombre)
+        .then(mensajeAgradecimiento)
+        .catch(mensajeError);
 }
 
 
-function calcular(){
-    try{
-        valorPantalla = eval(valorPantalla).toString();
-        actualizarPantalla();
+function enviarDatosAlServidor(nombre) {
+    return new Promise((resolve, reject) => {
 
-    }catch(e){
-        valorPantalla = 'Error';
-        actualizarPantalla();
-    }
+        setTimeout(() => {
+            const exito = (nombre.toLowerCase() != 'error');
+            if (exito) {
+                resolve('Bienvenido al sistema')
+            } else {
+                reject('Acceso denegado');
+            }
+
+        }, 2000);
+    });
 }
 
-function actualizarPantalla(){
-    document.getElementById('pantalla').value = valorPantalla;
+function mensajeAgradecimiento(mensaje){
+    const mensajeElement = document.getElementById('mensaje');
+    mensajeElement.textContent = mensaje;
+    mensajeElement.classList.remove('oculto');
+}
+
+function mensajeError(error){
+    const mensajeElement = document.getElementById ('mensaje');
+    mensajeElement.textContent = error;
+    mensajeElement.classList.remove('oculto');
 }
